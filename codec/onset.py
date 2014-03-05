8911158af39cdd465357e456eedf63e39cf69012
AJ Ferrick <ajferrick@gmail.com>
2014-03-04 20:55:46 -0800

    Added the window state machine.

--- codec/onset.py
+++ codec/onset.py
@@ -12,7 +12,37 @@ def onset_in_block(signal):
     This function will output a Boolean value. '''
     N = signal.size
     thresh = 0.05
-    fft = np.fft.fft(block)[:N/2]
+    fft = np.fft.fft(signal)[:N/2]
     
     energy = np.sum(np.linspace(0, 1, N/2) * np.abs(fft)**2) / (N/2.)
     return energy > thresh
+
+
+class WindowState(object):
+
+    def __init__(self):
+        self.state = 0
+
+    def transient(self):
+        if self.state == 0:
+            self.state = 1
+        elif self.state == 1:
+            self.state = 2
+        elif self.state == 2:
+            self.state = 2
+        elif self.state == 3:
+            self.state = 2
+        return self.state
+
+    def no_transient(self):
+        if self.state == 0:
+            self.state = 0
+        elif self.state == 1:
+            self.state = 2
+        elif self.state == 2:
+            self.state = 3
+        elif self.state == 3:
+            self.state = 4
+        return self.state
+
+
