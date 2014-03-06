5af7d02aeedce201975acb8095774ee272f28277
AJ Ferrick <ajferrick@gmail.com>
2014-03-05 21:57:43 -0800

    KBD window is now vectorized.
    Speed-up achieves of like...15x in encoding time. Win.

--- codec/window.py
+++ codec/window.py
@@ -18,17 +18,18 @@ def HanningWindow(dataSampleArray):
 def KBDWindow(dataSampleArray, alpha=4.):
     ''' Performs a KBD window (with tuning param) on the sample '''
     N = float(len(dataSampleArray))
-    window = np.zeros(N)
-    s = 0.
+    
+    # Create the numerators for half the KBD
+    t = np.arange(int(N/2))
+    window = np.i0(np.pi * alpha * np.sqrt(1.-(4.*t/N-1.)**2))
+    window = np.cumsum(window)
 
-    for i in range(int(N/2)):
-        s += np.i0(np.pi * alpha * np.sqrt(1.-(4.*i/N-1.)**2))
-        window[i] = s
+    # Make sure to add the boundary value for the denominator calculation
+    den = window[-1] + np.i0(np.pi * alpha * np.sqrt(1.-(4.*N/2./N-1.)**2))
 
-    s += np.i0(np.pi * alpha * np.sqrt(1.-(4.*N/2./N-1.)**2))
-
-    for i in range(int(N/2)):
-        window[i] = np.sqrt(window[i]/s)
-        window[N-i-1] = window[i]
+    # Actually perform the operations to get the 'derived' nature
+    # And then mirror the output to get a symmetric window
+    window = np.sqrt(window/den)
+    window = np.concatenate((window, window[::-1]), axis=0)
 
     return dataSampleArray * window
