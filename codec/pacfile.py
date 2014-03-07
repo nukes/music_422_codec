d3c151c88741c487cfb746d8a4dbe1aa73628680
AJ Ferrick <ajferrick@gmail.com>
2014-03-07 00:27:23 -0800

    Pulled the Coding Parameters out of everything but the Encoder

--- codec/pacfile.py
+++ codec/pacfile.py
@@ -12,14 +12,11 @@ from provided.pcodec import Encode, EncodeSingleChannel, Decode
 from codec.psychoac import ScaleFactorBands, AssignMDCTLinesFromFreqLimits
 
 
-class PACFile(object):
+class PACReader(object):
 
     def __init__(self, filename):
-        self.filename = filename
+        self.file = open(filename, 'rb')
         self.tag = 'PAC '
-
-    def read_header(self):
-        ''' Read the header of the PAC file. '''
 
         # Ensure we are reading PAC files
         tag = self.file.read(4)
@@ -29,16 +26,12 @@ class PACFile(object):
         # Grab all the delicious metadata
         (a, b, c, d, e, f) = struct.unpack('<LHLLHH',
                                 self.file.read(struct.calcsize('<LHLLHH')))
-
-        # TODO: Remove the CodingParams once they are no longer needed
-        cp = CodingParams()
-        cp.sampleRate = a
-        cp.nChannels = b
-        cp.numSamples = c
-        cp.nMDCTLines = d
-        cp.nSamplesPerBlock = d
-        cp.nScaleBits = e
-        cp.nMantSizeBits = f
+        self.sample_rate = a
+        self.channels = b
+        self.mdct_lines = d
+        self.samples_per_block = d
+        self.scale_bits = e
+        self.mant_bits = f
 
         # Compute the scale factor bands
         bands = struct.unpack('<L', self.file.read(struct.calcsize('<L')))[0]
@@ -48,18 +41,15 @@ class PACFile(object):
 
         # Prime the previous block needed for the overlap-and-add
         prev_block = []
-        for ch in range(cp.nChannels):
-            prev_block.append(np.zeros(cp.nMDCTLines, dtype=np.float64))
+        for ch in range(self.channels):
+            prev_block.append(np.zeros(self.mdct_lines, dtype=np.float64))
         self.overlap_block = prev_block
-        cp.overlapAndAdd = prev_block
 
-        return cp
-
-    def read_block(self, channels, mdct_lines, scale_bits, mant_bits):
+    def read_block(self):
         ''' Read a block of data from the PAC file. '''
 
         data = []
-        for ch in range(channels):
+        for ch in range(self.channels):
 
             # Figure out how much data is in this block
             # TODO: This is where we would catch different window sizes in block-switching
@@ -86,14 +76,14 @@ class PACFile(object):
             # TODO: This is where we would extract the window size for block switching
             bit_alloc = []
             scale_factor = []
-            mant = np.zeros(mdct_lines, dtype=np.int32)
+            mant = np.zeros(self.mdct_lines, dtype=np.int32)
             
-            overall_scale = pb.ReadBits(scale_bits)
+            overall_scale = pb.ReadBits(self.scale_bits)
             for band in range(self.scale_factors.nBands):
-                alloc = pb.ReadBits(mant_bits)
+                alloc = pb.ReadBits(self.mant_bits)
                 alloc += 1 if alloc else 0
                 bit_alloc.append(alloc)
-                scale_factor.append(pb.ReadBits(scale_bits))
+                scale_factor.append(pb.ReadBits(self.scale_bits))
 
                 # If there are bits allocated to this band, extract the
                 # mantiassas and place them into the mantissa array
@@ -107,90 +97,26 @@ class PACFile(object):
 
             # TODO: Eliminate the Coding Params when we can
             cp = CodingParams()
-            cp.nMDCTLines = mdct_lines
-            cp.nScaleBits = scale_bits
+            cp.nMDCTLines = self.mdct_lines
+            cp.nScaleBits = self.scale_bits
             cp.sfBands = self.scale_factors
 
             # Decode the data
             decoded = Decode(scale_factor, bit_alloc, mant, overall_scale, cp)
-            data[ch] = np.concatenate([data[ch], np.add(self.overlap_block[ch], decoded[:mdct_lines])])
-            self.overlap_block[ch] = decoded[mdct_lines:]
+            data[ch] = np.concatenate([data[ch], np.add(self.overlap_block[ch], decoded[:self.mdct_lines])])
+            self.overlap_block[ch] = decoded[self.mdct_lines:]
 
-        # Return both the data and the next block needed for the overlap-and-add
         return data
 
-    def write_data(self, data, sample_rate, channels, samples, mdct_lines, scale_bits, mant_bits, target_bps):
-        ''' Write a block of signed float data in the range of -1...1 to the
-        PACfile the object holds a reference to.
-        '''
+    def close(self):
+        self.file.close()
 
-        # Construct the full block to write out
-        full_block = []
-        for ch in range(channels):
-            block = np.concatenate([self.previous_block[ch], data[ch]])
-            full_block.append(block)
-        self.previous_block = data
 
-        # TODO: Remove the CodingParams once they are no longer needed
-        cp = CodingParams()
-        cp.sampleRate = sample_rate
-        cp.nChannels = channels
-        cp.nMDCTLines = mdct_lines
-        cp.nScaleBits = scale_bits
-        cp.nMantSizeBits = mant_bits
-        cp.sfBands = self.scale_factors
-        cp.targetBitsPerSample = target_bps
+class PACWriter(object):
 
-        # TODO: Move this out of the file. This is retarded.
-        (scale_factor, bit_alloc, mant, overall_scale) = Encode(full_block, cp)
-
-        # Write the encoded data to the file
-        for ch in range(channels):
-
-            # Determine the size of the channel's block
-            bits = scale_bits
-            for band in range(self.scale_factors.nBands):
-                bits += mant_bits + scale_bits
-                if bit_alloc[ch][band]:
-                    bits += bit_alloc[ch][band] * self.scale_factors.nLines[band]
-
-            # TODO: This is where we count the bits needed for block switching
-            # i.e. This is where we add '2' to the count of bits needed
-
-            # Convert the bits to bytes, using the conventional definition of
-            # 8 bits to 1 byte.  Add a "spillover" byte if we are just shy of
-            # the 8 byte boundary -- i.e. guarantee we have the byte capacity
-            bytes = bits / 8 + int(bits % 8 != 0)
-
-            # Write out the size of the data block
-            out = struct.pack('<L', int(bytes))
-            self.file.write(out)
-
-            # TODO: Eliminate this PackedBits if possible. Seems hard, though.
-            pb = PackedBits()
-            pb.Size(bytes)
-
-            # Actually pack the data
-            pb.WriteBits(overall_scale[ch], scale_bits)
-            i_mant = 0
-            for band in range(self.scale_factors.nBands):
-                alloc = bit_alloc[ch][band]
-                if alloc:
-                    alloc -= 1
-                pb.WriteBits(alloc, mant_bits)
-                pb.WriteBits(scale_factor[ch][band], scale_bits)
-                if bit_alloc[ch][band]:
-                    for i in range(self.scale_factors.nLines[band]):
-                        pb.WriteBits(mant[ch][i_mant+i], bit_alloc[ch][band])
-                    i_mant += self.scale_factors.nLines[band]
-
-            # TODO: This is where we would pack in the block switching data
-
-            # Finally, write this damned data to the file
-            self.file.write(pb.GetPackedData())
-
-    def write_header(self, sample_rate, channels, samples, mdct_lines, scale_bits, mant_bits):
-        ''' Accept a lot of bullshit to write the header out. '''
+    def __init__(self, filename, sample_rate, channels, samples, mdct_lines, scale_bits, mant_bits, target_bps):
+        self.file = open(filename, 'wb')
+        self.tag = 'PAC '
         self.file.write(self.tag)
 
         # Ensure that the number of samples in the file is a multiple of the
@@ -221,11 +147,84 @@ class PACFile(object):
         for ch in range(channels):
             self.previous_block.append(np.zeros(mdct_lines, dtype=np.float64))
 
-    def open_to_write(self):
-        self.file = open(self.filename, 'wb')
+        # Save the rest of the parameters so we can use them when writing
+        self.sample_rate = sample_rate
+        self.channels = channels
+        self.samples = samples
+        self.mdct_lines = mdct_lines
+        self.scale_bits = scale_bits
+        self.mant_bits = mant_bits
+        self.target_bps = target_bps
 
-    def open_to_read(self):
-        self.file = open(self.filename, 'rb')
+    def write_data(self, data):
+        ''' Write a block of signed float data in the range of -1...1 to the
+        PACfile the object holds a reference to.
+        '''
+
+        # Construct the full block to write out
+        full_block = []
+        for ch in range(self.channels):
+            block = np.concatenate([self.previous_block[ch], data[ch]])
+            full_block.append(block)
+        self.previous_block = data
+
+        # TODO: Remove the CodingParams once they are no longer needed
+        cp = CodingParams()
+        cp.sampleRate = self.sample_rate
+        cp.nChannels = self.channels
+        cp.nMDCTLines = self.mdct_lines
+        cp.nScaleBits = self.scale_bits
+        cp.nMantSizeBits = self.mant_bits
+        cp.sfBands = self.scale_factors
+        cp.targetBitsPerSample = self.target_bps
+
+        # TODO: Move this out of the file. This is retarded.
+        (scale_factor, bit_alloc, mant, overall_scale) = Encode(full_block, cp)
+
+        # Write the encoded data to the file
+        for ch in range(self.channels):
+
+            # Determine the size of the channel's block
+            bits = self.scale_bits
+            for band in range(self.scale_factors.nBands):
+                bits += self.mant_bits + self.scale_bits
+                if bit_alloc[ch][band]:
+                    bits += bit_alloc[ch][band] * self.scale_factors.nLines[band]
+
+            # TODO: This is where we count the bits needed for block switching
+            # i.e. This is where we add '2' to the count of bits needed
+
+            # Convert the bits to bytes, using the conventional definition of
+            # 8 bits to 1 byte.  Add a "spillover" byte if we are just shy of
+            # the 8 byte boundary -- i.e. guarantee we have the byte capacity
+            bytes = bits / 8 + int(bits % 8 != 0)
+
+            # Write out the size of the data block
+            out = struct.pack('<L', int(bytes))
+            self.file.write(out)
+
+            # TODO: Eliminate this PackedBits if possible. Seems hard, though.
+            pb = PackedBits()
+            pb.Size(bytes)
+
+            # Actually pack the data
+            pb.WriteBits(overall_scale[ch], self.scale_bits)
+            i_mant = 0
+            for band in range(self.scale_factors.nBands):
+                alloc = bit_alloc[ch][band]
+                if alloc:
+                    alloc -= 1
+                pb.WriteBits(alloc, self.mant_bits)
+                pb.WriteBits(scale_factor[ch][band], self.scale_bits)
+                if bit_alloc[ch][band]:
+                    for i in range(self.scale_factors.nLines[band]):
+                        pb.WriteBits(mant[ch][i_mant+i], bit_alloc[ch][band])
+                    i_mant += self.scale_factors.nLines[band]
+
+            # TODO: This is where we would pack in the block switching data
+
+            # Finally, write this damned data to the file
+            self.file.write(pb.GetPackedData())
 
     def close(self):
         self.file.close()
