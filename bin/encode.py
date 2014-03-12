4fd6050626a813a35426e7e4f2981e1972c340db
AJ Ferrick <ajferrick@gmail.com>
2014-03-11 17:44:17 -0700

    Revert "OK Final Fix"
    This reverts commit e7d2ff85cba8cf00f9ac5ec358965527241c4b96.

--- bin/encode.py
+++ bin/encode.py
@@ -8,7 +8,7 @@ from codec.pacfile import PACWriter
 
 elapsed = time.time()
 
-buf = TransientBuffer('SQAM/cast_1.wav')
+buf = TransientBuffer('SQAM/gspi35_1.wav')
 
 channels = 2
 sample_rate = 44100
